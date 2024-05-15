f7de6f2e3999f2c2f21abf0b27dee16e65d14f67
flint <dionarfian99@gmail.com>
2024-05-15 22:40:26 +0700

    update mobile

---
+++ hero.php
@@ -0,0 +1,46 @@
+
+<div id="pc-section" class="p-0 d-none d-lg-block">	
+	<div id="carousel-na" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner parent-container-img">
+            <div class="carousel-item active">
+                    <div class="slide" style="background-image: url('assets/coverrev3.jpg');">
+                    </div>
+            </div>
+            <div class="carousel-item">
+                    <div class="slide" style="background-image: url('assets/coverrev4.jpg');">
+                    </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-na" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-na" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
+<div id="mobile-section" class="p-0 d-block d-lg-none"> 
+    <div id="carousel-mobile" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner parent-container-img">
+            <div class="carousel-item active">
+                    <div class="slide" style="background-image: url('assets/mobile/sampulpotrait1.jpg');">
+                    </div>
+            </div>
+            <div class="carousel-item">
+                    <div class="slide" style="background-image: url('assets/mobile/sampulpotrait2.jpg');">
+                    </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-mobile" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-mobile" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,461 @@
+<!-- /*
+* Bootstrap 5
+* Template Name: Furni
+* Template Author: Untree.co
+* Template URI: https://untree.co/
+* License: https://creativecommons.org/licenses/by/3.0/
+*/ -->
+<!doctype html>
+<html lang="en">
+
+	<?php include 'head.php'; ?>
+
+	<?php include 'config.php'; ?>
+
+	<body>
+
+		<?php include 'nav.php'; ?>	
+
+		<?php include 'hero.php'; ?>
+
+		<div class="container-fluid mt-3 mb-5">
+		  <h2 style="color: #826f5f;">RATTAN CANE WEBBING</h2>
+		   <section class="customer-logos slider">
+		      <div class="slide">
+		      	<img src="images/openrattan.png" alt="openrattan">
+		      	<p class="desc-slide">Open Hexagonal Rattan Webbing</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/squarerattan.png" alt="squarerattan">
+		      	<p class="desc-slide">Square Rattan Webbing</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/closerattan.png" alt="closerattan">
+		      	<p class="desc-slide">Close Rattan Webbing</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/closexita.png" alt="closexita">
+		      	<p class="desc-slide">Close Xita Rattan Webbing</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/flower.png" alt="flower">
+		      	<p class="desc-slide">Flower Rattan Webbing</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/focrat.png" alt="focrat">
+		      	<p class="desc-slide">FOC (Flat Oval Core)</p>
+		      </div>
+		      <div class="slide">
+		      	<img src="images/mita4row.png" alt="mita4row">
+		      	<p class="desc-slide">Close Mita 4 Rows</p>
+		      </div>
+		   </section>
+		</div>
+
+		<div class="container-fluid text-center p-0 d-none d-lg-block">
+	    	<img src="assets/gambardipagehome.jpg" style="max-width:100%;" alt="gambar rotan banyak">
+	    </div>
+
+	    <div class="container-fluid text-center p-0 d-block d-lg-none">
+	    	<img src="assets/mobile/crafted.jpg" style="max-width:100%;" alt="gambar rotan banyak">
+	    </div>
+
+		<!-- Start We Help Section -->
+		<div class="p-0 d-none d-lg-block">
+			<div class="we-help-section container-fluid">
+				<div class="row justify-content-between">
+					<div class="container-fluid col-lg-12 col-md-10 col-sm-12 mb-5 mb-lg-0" style="padding-left: 100px;padding-right: 100px;">
+						<div class="imgs-grid">
+							<div class="grid grid-1 col-lg-9" style="background-color: lightgray;border-radius: 3%;">
+								<h2 class="section-title mt-4" style="color: #826f5f; text-align: left;">
+									RATTAN LAMP SHADES
+								</h2>
+								<p style="padding-left: 20px; padding-right: 20px; margin-top: -3%;">
+									Rattan lampshades make them a popular choice for a variety of
+									furniture and decorations. It comes in a variety of shapes, sizes and
+									styles, making it versatile enough to complement a variety of interior
+									design themes, from bohemian and tropical to modern and minimalist.
+									Can custom design according to request.
+								</p>
+
+								<p style="padding-left: 20px;"><a href="https://wa.me/6281292314213?text=Hello! we want to know about ..." class="btn btn-custom" style="background-color:#7a7a7a;" target="blank__">More Info</a></p>
+							</div>
+							<div class="grid grid-3"><img src="assets/rattanlampshade.jpg" alt="Untree.co"></div>
+							<!-- <div class="grid grid-3"><img src="assets/rattanlampshade.jpg" alt="Untree.co"></div> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-0 d-block d-lg-none" id="mobile-section">
+			<div class="container-fluid mt-3">
+				<div class="row">
+					<div class="container-fluid">
+						<div class="row justify-content-center">
+						    <div class="col-md-3 mb-3">
+					    		<div class="card mb" style="background-color: lightgrey;">
+					              <div class="card-body mb">
+					                <h4 class="section-title mb-3" style="color: #826f5f; text-align: left;">
+					                	RATTAN LAMP SHADES
+					                </h4>
+					                <p style="text-align: justify;">
+						                Rattan lampshades make them a popular choice for a variety of
+						                furniture and decorations. It comes in a variety of shapes, sizes and styles, making it versatile enough to complement a variety of interior design themes, from bohemian and tropical to modern and minimalist.
+						                Can custom design according to request.
+						            </p>
+					                
+					                <p>
+					                    <a href="https://wa.me/6281292314213?text=Hello! we want to know about ..." class="btn btn-custom" style="background-color:#826f5f;" target="_blank">More Info
+					                    </a>
+					                </p>
+					              </div>
+					              <img class="card-img-top" src="assets/rattanlampshade.jpg" alt="Card image cap">
+					            </div>
+					        </div>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End We Help Section -->
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="container-fluid">
+					<div class="row justify-content-center">
+					    <div class="col-md-3 mb-3">
+				    		<div class="card mb" style="background-color: lightgrey;">
+				              <img class="card-img-top" src="assets/basket.jpg" alt="Card image cap">
+				              <div class="card-body mb">
+				                <h5 class="card-title">BASKET</h5>
+				                <a type="button">View Product</a>
+				              </div>
+				            </div>
+				        </div>
+				        <div class="col-md-3 mb-3">
+				    		<div class="card mb" style="background-color: lightgrey;">
+				              <img class="card-img-top" src="assets/placemate.jpg" alt="Card image cap">
+				              <div class="card-body mb">
+				                <h5 class="card-title">PLACEMATE</h5>
+				                <a type="button">View Product</a>
+				              </div>
+				            </div>
+				        </div>
+				        <div class="col-md-3">
+				    		<div class="card mb" style="background-color: lightgrey;">
+				              <img class="card-img-top" src="assets/mirror.jpg" alt="Card image cap">
+				              <div class="card-body mb">
+				                <h5 class="card-title">MIRROR</h5>
+				                <a type="button">View Product</a>
+				              </div>
+				            </div>
+				        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Start We Help Section -->
+		<div class="we-help-section container-fluid p-0 d-none d-lg-block mb-5">
+			<div class="row justify-content-between">
+				<div class="container-fluid col-lg-12 col-md-10 col-sm-12 mt-5 mb-lg-0">
+					<div class="imgs-grid">
+						<div class="container-fluid grid grid-1 col-lg-9">
+							<img src="assets/rattanfurniture.jpg" alt="rattan">
+						</div>
+						<div class="container-fluid grid grid-3 col-lg-10" style="background-color: lightgray;border-radius: 3%;">
+							<h2 class="section-title" style="color: #826f5f; text-align: left;">
+								RATTAN FURNITURE
+							</h2>
+							<p style="padding-left: 20px; padding-right: 20px; margin-top: -3%;">
+								Rattan furniture is made from rattan trees due to its strength, durability and flexibility. It has a warm and inviting appearance, lending a touch of tropical charm to any room and making it a timeless choice for interior and exterior decoration.
+								Can custom design according to request
+							</p>
+
+							<p style="padding-left: 20px;">
+								<a href="https://wa.me/6281292314213?text=Hello! we want to know about ..." class="btn btn-custom" style="background-color:#826f5f;" target="blank__">More Info
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-0 d-block d-lg-none" id="mobile-section">
+			<div class="container-fluid mt-3">
+				<div class="row">
+					<div class="container-fluid">
+						<div class="row justify-content-center">
+						    <div class="col-md-3 mb-3">
+					    		<div class="card mb" style="background-color: lightgrey;">
+					              <div class="card-body mb">
+					                <h4 class="section-title mb-3" style="color: #826f5f; text-align: left;">RATTAN FURNITURE</h4>
+					                <p style="text-align: justify;">Rattan furniture is made from rattan trees due to its strength, durability, and flexibility. It has a warm and inviting appearance, lending a touch of tropical charm to any room and making it a timeless choice for interior and exterior decoration. Can custom design according to request</p>
+					                
+					                <p>
+					                    <a href="https://wa.me/6281292314213?text=Hello! we want to know about ..." class="btn btn-custom" style="background-color:#826f5f;" target="_blank">More Info
+					                    </a>
+					                </p>
+					              </div>
+					              <img class="card-img-top" src="assets/rattanfurniture.jpg" alt="Card image cap">
+					            </div>
+					        </div>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- End We Help Section -->
+		<div class="p-0 d-none d-lg-block">
+		    <div class="row mb-1">
+				<div class="brown-bg text-center fw-bold mb-3">
+					<h2 class="mb-5 mt-5" style="color: #826f5f;">WHY CHOOSE US?</h2>
+
+					<div class="row col-12 justify-content-center mb-5">
+						<div class="col-4" style="text-align: justify;">
+							<h5 class="choose-title">Quality</h5>
+							<p class="choose-desc mb-5">
+								Having a Quality Control team in each production
+								line, to ensure that the quality of the products we
+								sell is always consistent with our standards
+							</p>
+
+							<h5 class="choose-title">Management Team</h5>
+							<p class="choose-desc">
+								Having an expert and professional management team
+								to provide the best service to all of our customers
+							</p>
+						</div>
+
+						<div class="col-4" style="text-align: justify;">
+							<h5 class="choose-title">Competitive</h5>
+							<p class="choose-desc mb-4">
+								Ensuring that the prices we offer can compete in the
+								global market, so we can provide the best prices for
+								each of our customers, especially B2B (Business to
+								Business) scale companies
+							</p>
+
+							<h5 class="choose-title">Custom Order</h5>
+							<p class="choose-desc">
+								Receive special orders from customers who have
+								different designs from the products we sell
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-0 d-block d-lg-none" id="mobile-section">
+			<div class="brown-bg-mobile text-center fw-bold mb-3">
+				<h2 class="mb-3 mt-5" style="color: #826f5f;">WHY CHOOSE US?</h2>
+
+				<div class="col-12 justify-content-center">
+					<div class="col-12" style="text-align: justify;">
+						<h5 class="choose-title">Quality</h5>
+						<p class="choose-desc mb-3">
+							Having a Quality Control team in each production
+							line, to ensure that the quality of the products we
+							sell is always consistent with our standards
+						</p>
+
+						<h5 class="choose-title">Management Team</h5>
+						<p class="choose-desc">
+							Having an expert and professional management team
+							to provide the best service to all of our customers
+						</p>
+					</div>
+
+					<div class="col-12" style="text-align: justify;">
+						<h5 class="choose-title">Competitive</h5>
+						<p class="choose-desc mb-3">
+							Ensuring that the prices we offer can compete in the
+							global market, so we can provide the best prices for
+							each of our customers, especially B2B (Business to
+							Business) scale companies
+						</p>
+
+						<h5 class="choose-title">Custom Order</h5>
+						<p class="choose-desc">
+							Receive special orders from customers who have
+							different designs from the products we sell
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid mt-5">
+			<div class="row justify-content-between">
+				<div class="col-lg-4 ps-lg-5">
+					<h3 class="section-title" style="color: #826f5f; text-align: left;">NEWS & ARTICLE</h3>
+					<p style="text-align: left;">
+						Follow and read our articles to find out about rattan cane webbing and others.
+					</p>
+				</div>
+				<div class="row col-lg-8 mb-5 mb-lg-0">
+					<?php foreach ($blog as $blog) { ?>
+						<div class="col-6">
+							<div class="card">
+								<div class="card-body text-center">
+									<a href="">
+										<img src="<?=$defaulturl?>newsportal/admin/postimages/<?=$blog['PostImage']?>" style="width:100%;">
+									</a>
+									<!-- <ul class="list-inline mt-3">
+				                        <li class="list-inline-item"><i class="fas fa-user"></i> <?=$blog['postedBy']?></li>
+				                        <li class="list-inline-item"><i class="far fa-clock"></i> <?=$blog['PostingDate']?></li>
+				                    </ul> -->
+				                    <hr>
+				                    <p class="lead"><?=$blog['PostTitle']?></p>
+				                    <a class="btn btn-outline-dark my-2" href="<?=$defaulturl?>newsportal/news-details.php?nid=<?=$blog['id']?>" role="button">Read more...</a>
+								</div>
+							</div>
+						</div>
+					<?php } ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="we-help-section container-fluid" style="background-color: #EEF3EF"> 			
+			<h3 class="text-center mb-5" style="color: #826f5f; letter-spacing: 10px;">GALLERY</h3>
+			<div class="row">
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/galeri1.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/galeri1.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/14.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/14.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/galeri2.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/galeri2.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/galeri4.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/galeri4.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/galeri5.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/galeri5.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/11.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/11.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/12.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/12.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+	                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+	                   data-image="assets/galeri/galeri15.jpg"
+	                   data-target="#image-gallery">
+	                    <img class="img-thumbnail"
+	                         src="assets/galeri/galeri15.jpg"
+	                         alt="Another alt text">
+	                </a>
+	            </div>
+	        </div>
+	        <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	            <div class="modal-dialog modal-lg">
+	                <div class="modal-content">
+	                    <div class="modal-header">
+	                        <h4 class="modal-title" id="image-gallery-title"></h4>
+	                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span>
+	                        </button>
+	                    </div>
+	                    <div class="modal-body">
+	                        <img id="image-gallery-image" class="img-responsive col-md-12" src="">
+	                    </div>
+	                    <div class="modal-footer">
+	                        <button type="button" class="btn btn-secondary float-left" id="show-previous-image"><i class="fa fa-arrow-left"></i>
+	                        </button>
+
+	                        <button type="button" id="show-next-image" class="btn btn-secondary float-right"><i class="fa fa-arrow-right"></i>
+	                        </button>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+		
+		<?php include 'footer.php'; ?>	
+
+		<script src="js/bootstrap.bundle.min.js"></script>
+		<script src="js/tiny-slider.js"></script>
+		<script src="js/custom.js"></script>
+		<script src="js/galeri.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+			    $('.customer-logos').slick({
+			        slidesToShow: 3,
+			        slidesToScroll: 1,
+			        autoplay: true,
+			        autoplaySpeed: 1500,
+			        arrows: false,
+			        dots: false,
+			        pauseOnHover: false,
+			        responsive: [{
+			            breakpoint: 768,
+			            settings: {
+			                slidesToShow: 2
+			            }
+			        }, {
+			            breakpoint: 520,
+			            settings: {
+			                slidesToShow: 1
+			            }
+			        }]
+			    });
+			});
+		</script>
+		<!--<script>
+			document.addEventListener("contextmenu", function(e){
+				e.preventDefault();
+			}, false);
+		</script>-->
+	</body>
+
+</html>
