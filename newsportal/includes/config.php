314671576d8b0008c8d3739f6ad2200aff3f70bf
flint <dionarfian99@gmail.com>
2024-06-18 23:57:22 +0700

    update sekaone V2

--- newsportal/includes/config.php
+++ newsportal/includes/config.php
@@ -20,7 +20,7 @@ define('DB_PASS' ,'1234Harusdigunakan!');
 define('DB_NAME','u752324154_sekaone');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
-$defaulturl  = "http://sekaone.test/";
+$defaulturl  = "https://multisekawan.com/";
 // Check connection
 if (mysqli_connect_errno())
 {
