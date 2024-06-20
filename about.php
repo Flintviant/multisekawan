475fa715ea8132ad15d27f8527f5f110c96835ef
flint <dionarfian99@gmail.com>
2024-06-20 23:43:24 +0700

    update about us

--- about.php
+++ about.php
@@ -17,225 +17,252 @@
 		<?php include 'hero.php'; ?>
 
 		<!-- Start Team Section -->
-		<div class="container">
-			<div class="untree_co-section">
-				<div class="container-fluid">
+		
+		<div class="untree_co-section">
+			<div class="container-fluid">
 
-					<div class="row">
-						<div class="col-12 col-md-6 col-lg-6 mb-md-0">
-							<h2 class="section-title" style="font-family: ; text-align: left; color: #75614f;">ABOUT US</h2>
-						</div>
+				<div class="container-fluid row">
+					<div class="col-12 col-md-6 col-lg-6 mb-md-0">
+						<h2 class="section-title" style="font-family: ; text-align: left; color: #75614f;">ABOUT US</h2>
 					</div>
+				</div>
 
-					<div class="container row">
-						<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
-							<h5 style="font-family: thin;color: #75614f;">
-								PT Multi Sekawan Internasional is an leading company that carries
-								out business activities by selling various kinds of Rattan products
-								such as Rattan Cane Webbing, Rattan Furniture, Home Decorations etc.
-								<br><br>
-								We have collaborated with craftmen in our region to create quality
-								products that are competitive in the world market. Large-scale and
-								integrated operational activities support our team in ensuring
-								supply chain efficiency and sustainability. With our extensive
-								knowledge and expertise in the field, our company has earned a
-								strong reputation for providing the best products and outstanding
-								customer service.
-							</h5>
-						</div>
-						<div class="col-12 col-md-6 col-lg-6 mb-md-0">
-							<img src="assets/sekawanbuilding.jpg" style="width:90%;" class="img-fluid">
-						</div>
+				<div class="container-fluid row">
+					<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
+						<h5 style="font-family: thin;color: #75614f;">
+							PT Multi Sekawan Internasional is an leading company that carries
+							out business activities by selling various kinds of Rattan products
+							such as Rattan Cane Webbing, Rattan Furniture, Home Decorations etc.
+							<br><br>
+							We have collaborated with craftmen in our region to create quality
+							products that are competitive in the world market. Large-scale and
+							integrated operational activities support our team in ensuring
+							supply chain efficiency and sustainability. With our extensive
+							knowledge and expertise in the field, our company has earned a
+							strong reputation for providing the best products and outstanding
+							customer service.
+						</h5>
 					</div>
-
-					<div class="row">
-						<div class="col-12 col-md-6 col-lg-6 mb-md-0">
-							<h2 class="section-title" style="color: #75614f;text-align: left;">Our Mission</h2>
-						</div>
+					<div class="col-12 col-md-6 col-lg-6 mb-md-0">
+						<img src="assets/sekawanbuilding.jpg" style="width:90%;" class="img-fluid">
 					</div>
+				</div>
 
-					<div class="">
-						<div class="col-12 col-md-6 col-lg-10 mb-5 mb-md-0">
-							<h5 style="color: #75614f; font-family: thin;">
-								To be the best professional, innovative and trusted company
-							</h5>
-						</div>
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-6 mb-md-0">
+						<h2 class="section-title" style="color: #75614f;text-align: left;">Our Mission</h2>
 					</div>
+				</div>
 
-					<div class="row">
-						<div class="col-12 col-md-6 col-lg-6 mb-md-0">
-							<h2 class="section-title" style="color: #75614f;text-align: left;">Our Vission</h2>
-						</div>
+				<div class="">
+					<div class="col-12 col-md-6 col-lg-10 mb-5 mb-md-0">
+						<h5 style="color: #75614f; font-family: thin;">
+							To be the best professional, innovative and trusted company
+						</h5>
 					</div>
+				</div>
 
-					<div class="mb-5">
-						<div class="col-12 col-md-6 col-lg-10 mb-5 mb-md-0">
-							<h5 style="color: #75614f; font-family: thin;">
-								<ul>
-									<li> Building an international scale company by upholding the principles of business ethics </li>
-									<li> Maintain integrity by having competent human resources </li>
-									<li> Prioritizing customer satisfaction by providing quality products and competitive prices </li>
-									<li> Build good and profitable long-term cooperative relationships with stakeholders </li>
-								</ul>
-							</h5>
-						</div>
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-6 mb-md-0">
+						<h2 class="section-title" style="color: #75614f;text-align: left;">Our Vission</h2>
 					</div>
+				</div>
 
-					<div class="container-fluid text-center text-white fw-bold mb-3">
-						<h2 style="color: #75614f;">OUR CORE VALUE</h2>
+				<div class="mb-5">
+					<div class="col-12 col-md-6 col-lg-10 mb-5 mb-md-0">
+						<h5 style="color: #75614f; font-family: thin;">
+							<ul>
+								<li> Building an international scale company by upholding the principles of business ethics </li>
+								<li> Maintain integrity by having competent human resources </li>
+								<li> Prioritizing customer satisfaction by providing quality products and competitive prices </li>
+								<li> Build good and profitable long-term cooperative relationships with stakeholders </li>
+							</ul>
+						</h5>
 					</div>
+				</div>
 
-					<div class="row justify-content-between mb-5">
-						<div class="col-lg-12">
-							<div class="row my-5 text-center">
-								<div class="col-12 col-md-4">
-									<div class="">
-										<div class="icon">
-											<img src="assets/icon/quality.png" style="width:80px;" alt="Image" class="imf-fluid">
-										</div>
-										<h3 style="color: #75614f;">Quality</h3>
-										<p style="color: #75614f;">
-											Acting fairly, honestly, and with <br>
-											responsibility and discipline are <br>
-											things that we strive for at every step
-										</p>
+				<div class="container-fluid text-center text-white fw-bold">
+					<h2 style="color: #75614f;">OUR CORE VALUE</h2>
+				</div>
+
+				<div class="row justify-content-between">
+					<div class="col-lg-12">
+						<div class="row my-5 text-center">
+							<div class="col-12 col-md-4">
+								<div class="">
+									<div class="icon">
+										<img src="assets/icon/qualityy.png" style="width:80px;" alt="Image" class="imf-fluid">
 									</div>
+									<h3 style="color: #75614f;font-family: thin;">Quality</h3>
+									<p style="color: #75614f;font-family: thin;">
+										Acting fairly, honestly, and with <br>
+										responsibility and discipline are <br>
+										things that we strive for at every step
+									</p>
 								</div>
+							</div>
 
-								<div class="col-12 col-md-4">
-									<div class="">
-										<div class="icon">
-											<img src="assets/icon/commitment.png" style="width:80px;" alt="Image" class="imf-fluid">
-										</div>
-										<h3 style="color: #75614f;">Commitment</h3>
-										<p style="color: #75614f;">
-											Consistent in providing the best service <br>
-											to all of our stakeholders
-										</p>
+							<div class="col-12 col-md-4">
+								<div class="">
+									<div class="icon">
+										<img src="assets/icon/commitmentt.png" style="width:80px;" alt="Image" class="imf-fluid">
 									</div>
+									<h3 style="color: #75614f;font-family: thin;">Commitment</h3>
+									<p style="color: #75614f;font-family: thin;">
+										Consistent in providing the best service <br>
+										to all of our stakeholders
+									</p>
 								</div>
+							</div>
 
-								<div class="col-12 col-md-4">
-									<div class="">
-										<div class="icon">
-											<img src="assets/icon/contribution.png" style="width:80px;" alt="Image" class="imf-fluid">
-										</div>
-										<h3 style="color: #75614f;">Contribution</h3>
-										<p style="color: #75614f;">
-											Dedicated to having a positive impact <br>
-											on all stakeholders and participating in <br>
-											global economic growth.
-										</p>
+							<div class="col-12 col-md-4">
+								<div class="">
+									<div class="icon">
+										<img src="assets/icon/contributionn.png" style="width:80px;" alt="Image" class="imf-fluid">
 									</div>
+									<h3 style="color: #75614f;font-family: thin;">Contribution</h3>
+									<p style="color: #75614f;font-family: thin;">
+										Dedicated to having a positive impact <br>
+										on all stakeholders and participating in <br>
+										global economic growth.
+									</p>
 								</div>
 							</div>
 						</div>
 					</div>
+				</div>
 
-					<div class="row mb-2">
-						<div class="container-fluid py-4 text-center fw-bold mb-3">
-							<h2 class="section-title" style="color: #75614f;">THE TEAM & MANAGEMENT</h2>
-						</div>
+				<div class="row">
+					<div class="container-fluid py-4 text-center fw-bold">
+						<h2 class="section-title" style="color: #75614f;">THE TEAM & MANAGEMENT</h2>
 					</div>
+				</div>
 
-					<!-- <div class="row text-center mb-5">
+				<!-- <div class="row text-center mb-5">
 
-						 Start Column 1
-						<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
-							<img src="assets/PANJI-1.png" style="width:50%;" class="img-fluid mb-5">
-							<h3 clas><a href="www/instagram.com/Panjibhskr" target="blank__"><span class="">A. Panji</span> Bhaskoro</a></h3>
-				            <span class="d-block position mb-4">Commissioner, Founder.</span>
-				            <p>
-				            	"A friendship founded on business is better than a business founded on friendship."
-				            </p>
-				            <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
-						</div> 
-						
-						<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
-							<img src="assets/DION-1.png" style="width:50%;" class="img-fluid mb-5">
+					 Start Column 1
+					<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
+						<img src="assets/PANJI-1.png" style="width:50%;" class="img-fluid mb-5">
+						<h3 clas><a href="www/instagram.com/Panjibhskr" target="blank__"><span class="">A. Panji</span> Bhaskoro</a></h3>
+			            <span class="d-block position mb-4">Commissioner, Founder.</span>
+			            <p>
+			            	"A friendship founded on business is better than a business founded on friendship."
+			            </p>
+			            <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
+					</div> 
+					
+					<div class="col-12 col-md-6 col-lg-6 mb-5 mb-md-0">
+						<img src="assets/DION-1.png" style="width:50%;" class="img-fluid mb-5">
 
-							<h3 clas><a href="www/instagram.com/dion_arfian14" target="blank__"><span class="">M. Arfian</span> Nugroho</a></h3>
-				            <span class="d-block position mb-4">Director, Founder.</span>
-				            <p>
-				            	"In The Proccess if you cant running, you can walking, if you cant walking, you can crawling, never stop moving."
-				            </p>
-				            <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
+						<h3 clas><a href="www/instagram.com/dion_arfian14" target="blank__"><span class="">M. Arfian</span> Nugroho</a></h3>
+			            <span class="d-block position mb-4">Director, Founder.</span>
+			            <p>
+			            	"In The Proccess if you cant running, you can walking, if you cant walking, you can crawling, never stop moving."
+			            </p>
+			            <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
 
-						</div> 
-						
+					</div> 
+					
 
-					</div> -->
+				</div> -->
 
-					<div class="product-section">
-						<div class="container-fluid">
-							<div class="row">
+				<div class="product-section">
+					<div class="container-fluid">
+						<div class="row g-1 row-cols-1 row-cols-lg-4 col-12 mx-auto">
 
-								<!-- Start Column 1 -->
-								<div class="col-12 col-md-4 col-lg-6 mb-5 mb-md-0">
-									<a class="product-item">
-										<img src="assets/icon/marketing.png" class="img-fluid product-thumbnail" style="width:150px;">
-										<h3 class="product-title">Marketing Departement</h3>
-										<h6>
-											Account Manager Department handle Sales & Marketing matters under Commercial Division. Every person in this team is a person in charge to their account (buyer).
-										</h6>
-									</a>
-								</div> 
-								<!-- End Column 1 -->
+							<!-- Start Column 1 -->
+							<div class="col-md-4">
+								<a class="product-item">
+									<img src="assets/icon/marketing-1.png" class="img-fluid product-thumbnail" style="width:150px;">
+									<h3 class="product-title text-uppercase" style="color:#75614f;">
+										<b>Marketing Departement</b></h3>
+									<h6>
+										Account Manager Department <br>
+										handle Sales & Marketing <br>
+										matters under Commercial <br>
+										Division. Every person in this <br>
+										team is a person in charge to <br>
+										their account (buyer).
+									</h6>
+								</a>
+							</div> 
+							<!-- End Column 1 -->
 
-								<!-- Start Column 2 -->
-								<div class="col-12 col-md-4 col-lg-6 mb-5 mb-md-0">
-									<a class="product-item">
-										<img src="assets/icon/research.jpg" class="img-fluid product-thumbnail" style="width:135px;">
-										<h3 class="product-title">Research Development</h3>
-										<h6>
-											A Product Development works under Commercial Division focus on develop new & existing product of SekaOne
-										</h6>
-									</a>
-								</div> 
-								<!-- End Column 2 -->
+							<!-- Start Column 2 -->
+							<div class="col-md-4">
+								<a class="product-item">
+									<img src="assets/icon/research-development.png" class="img-fluid product-thumbnail" style="width:150px;">
+									<h3 class="product-title text-uppercase" style="color:#75614f;">
+										<b>Research Development</b></h3>
+									<h6>
+										A Product Development works <br>
+										under Commercial Division focus <br>
+										on develop new & existing <br>
+										product of SekaOne
+									</h6>
+								</a>
+							</div> 
+							<!-- End Column 2 -->
 
-								<!-- Start Column 3 -->
-								<div class="col-12 col-md-4 col-lg-4 mb-5 mb-md-0">
-									<a class="product-item">
-										<img src="assets/icon/production.png" class="img-fluid product-thumbnail" style="width:150px;">
-										<h3 class="product-title">Production Departement</h3>
-										<h6>
-											This Department focus on Operational Support & Financial Matters. This department taking care Office Management, General Affairs, Legal & Accounting.
-										</h6>
-									</a>
-								</div>
-								<!-- End Column 3 -->
-
-								<!-- Start Column 4 -->
-								<div class="col-12 col-md-4 col-lg-4 mb-5 mb-md-0">
-									<a class="product-item">
-										<img src="assets/icon/finance.png" class="img-fluid product-thumbnail" style="width:130px;">
-										<h3 class="product-title">Operation & Financial</h3>
-										<h6>
-											This Department focus on Operational Support & Financial Matters. This department taking care Office Management, General Affairs, Legal & Accounting.
-										</h6>
-									</a>
-								</div>
-								<!-- End Column 4 -->
-
-								<!-- Start Column 5 -->
-								<div class="col-12 col-md-4 col-lg-4 mb-5 mb-md-0">
-									<a class="product-item">
-										<img src="assets/icon/shipment.jpg" class="img-fluid product-thumbnail" style="width:235px;">
-										<h3 class="product-title">Shipping Departement</h3>
-										<h6>
-											Shipping Department focus on Delivery & Shipping of the product
-										</h6>
-									</a>
-								</div>
-								<!-- End Column 5 -->
-
+							<!-- Start Column 3 -->
+							<div class="col-md-4">
+								<a class="product-item">
+									<img src="assets/icon/production-1.png" class="img-fluid product-thumbnail" style="width:150px;">
+									<h3 class="product-title text-uppercase" style="color:#75614f;">
+										<b>Production Departement</b></h3>
+									<h6>
+										This Department focus on <br>
+										Operational Support & Financial <br>
+										Matters. This department taking <br>
+										care Office Management, <br>
+										General Affairs, Legal & <br>
+										Accounting.
+									</h6>
+								</a>
 							</div>
+							<!-- End Column 3 -->
+
+						</div>
+
+						<div class="row g-1 row-cols-1 row-cols-lg-4 col-12 mx-auto">
+							<!-- Start Column 4 -->
+							<div class="col-md-6">
+								<a class="product-item">
+									<img src="assets/icon/operation-financial.png" class="img-fluid product-thumbnail" style="width:150px;">
+									<h3 class="product-title text-uppercase" style="color:#75614f;">
+										<b>Operation & Financial</b></h3>
+									<h6>
+										This Department focus on <br>
+										Operational Support & Financial <br>
+										Matters. This department taking <br>
+										care Office Management, <br>
+										General Affairs, Legal & <br>
+										Accounting.
+									</h6>
+								</a>
+							</div>
+							<!-- End Column 4 -->
+
+							<!-- Start Column 5 -->
+							<div class="col-md-6">
+								<a class="product-item">
+									<img src="assets/icon/shipment.png" class="img-fluid product-thumbnail" style="width:150px;">
+									<h3 class="product-title text-uppercase" style="color:#75614f;">
+										<b>Shipping Departement</b></h3>
+									<h6>
+										Shipping Department focus on <br>
+										Delivery & Shipping of the <br>
+										product
+									</h6>
+								</a>
+							</div>
+							<!-- End Column 5 -->
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+		
 		<!-- End Team Section -->
 		
 
